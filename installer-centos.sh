39b22036dbc003e133a8e933089a1b12724828e2
DeaDSouL <DeaDSouL@users.noreply.github.com>
2019-01-31 22:31:00 +0300

    Adding AutoLinux installer script for CentOS

---
+++ installer-centos.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+DIRSRC='/usr/local/src'
+ALS=('al.sysadmin-env' 'al.vim-env' 'al.docker' 'al.docker-network' 'al.docker-emby')
+
+cd "${DIRSRC}"
+
+yum -y update
+yum -y install git
+git clone https://github.com/DeaDSouL/AutoLinux.git
+
+
+for al in ${ALS[@]}; do
+    bash "${DIRSRC}/AutoLinux/CentOS/${al}" install
+done
+
